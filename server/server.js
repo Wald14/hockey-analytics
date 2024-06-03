1972b9cc046f5e8e253bd501f4eb5fdbcac86cf7
Wald14 <lukewald@hotmail.com>
2024-06-02 21:34:18 -0500

    updates to server again

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path')
-const cors = require('cors');
+// const cors = require('cors');
 
 const routes = require('./routes')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "..", 'client/build')));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", 'client/dist/index.html'))
+    res.sendFile(path.resolve( __dirname, "..", 'client/build/index.html'))
   })
 }
 
