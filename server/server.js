e9a72c55204f7da0fa49ffdaa41a48677364dc9e
Wald14 <lukewald@hotmail.com>
2024-06-02 21:26:24 -0500

    updates to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const path = require('path');
+const path = require('path')
 const cors = require('cors');
-const routes = require('./routes');
+
+const routes = require('./routes')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,15 +11,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+app.use(routes)
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "..", 'client/build')));
 
-// Catchall handler to serve the React app for any route
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", 'client/dist/index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
