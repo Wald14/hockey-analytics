54635c05cd45df7a9b6438a0163da96fb766aac9
Wald14 <lukewald@hotmail.com>
2024-06-02 21:15:29 -0500

    updates to server and main package.json files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const path = require('path')
 const fetch = require('node-fetch');
 const cors = require('cors');
 
-const path = require('path')
 const routes = require('./routes')
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -14,11 +14,16 @@ app.use(express.json());
 
 app.use(routes)
 
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "..", 'client/build')));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve( __dirname, "..", 'client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, "..", 'client/build/index.html'))
   })
 }
 
