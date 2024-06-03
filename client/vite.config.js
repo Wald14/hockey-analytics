906c81f00659d2745d6c8f017df279a227e2ec36
Wald14 <lukewald@hotmail.com>
2024-06-02 20:42:09 -0500

    Changes to hopefully fix deployment issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,9 +7,9 @@ import react from '@vitejs/plugin-react'
 // })
 
 export default defineConfig({
-  build: {
-    outDir: "build"
-  },
+  // build: {
+  //   outDir: "build"
+  // },
   server: {
     port: 5173,
     open: true,
