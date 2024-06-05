6dd60198ba2c96fd46eb2ed0bbe70535fccd279c
Wald14 <lukewald@hotmail.com>
2024-06-04 22:38:13 -0500

    Added sub nav bar to TeamRosterPage to swap between Roster and Stats

--- client/src/pages/TeamRosterPage.jsx
+++ client/src/pages/TeamRosterPage.jsx
@@ -1,19 +1,68 @@
 // React & React-Router-Dom
+import { useState } from 'react';
 import { useParams } from 'react-router-dom'
 
 // Components
-import {RosterInfo, RosterStats} from '../components'
+import { RosterInfo, RosterStats } from '../components'
+
+// Bootstrap
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 export default function TeamRosterPage() {
 
   const { teamAbbrev } = useParams()
 
+  const [selectedTab, setSelectedTab] = useState("roster")
+  const handleChange = (e) => { 
+    setSelectedTab(e.target.name) 
+    console.log(e.target.name)
+  }
+
   return (
     <div className='page-container'>
+
+
+      <div style={{ marginLeft: 'auto', marginRight: 'auto'}}>
+        <Row
+          style={{
+            borderBottom: "solid #0A74DA 1px",
+            paddingBottom: "2px",
+            marginTop: "16px"
+          }}
+        >
+          {/* <Col 
+            xs={4} md={2} lg={1}
+          > */}
+            <a
+            name="roster" 
+            className={`headerBtnA ${selectedTab === "roster" ? 'selected' : ''}`} onClick={handleChange} >
+              Roster
+            </a>
+          {/* </Col>
+          <Col 
+            xs={4} md={2} lg={1}
+          > */}
+            <a 
+            name="stats" 
+            className={`headerBtnA ${selectedTab === "stats" ? 'selected' : ''}`} onClick={handleChange} >
+              Stats
+              </a>
+              {/* </Col> */}
+        </Row>
+      </div>
+
+
       {/* <div className='page-sub-container'> */}
-        <RosterStats teamAbbrev={teamAbbrev} />
+      {selectedTab === "roster" &&
         <RosterInfo teamAbbrev={teamAbbrev} />
+      }
+      {selectedTab === "stats" &&
+        <RosterStats teamAbbrev={teamAbbrev} />
+      }
       {/* </div> */}
     </div>
   )
